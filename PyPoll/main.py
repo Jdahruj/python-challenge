67e3ac18d2e6ba9b7501a3d28d07640daec8e6ec
Joe Dahruj <joedahruj@me.com>
2020-03-16 18:37:09 -0400

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,22 @@
-{\rtf1}
+import os
+import csv
+import pandas
+
+csvpath = os.path.join('budget_data.csv')
+
+# Method 2: Improved Reading using CSV module
+
+with open(csvpath) as csvfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    print(csvreader)
+
+    # Read the header row first (skip this step if there is now header)
+    csv_header = next(csvreader)
+    print(f"CSV Header: {csv_header}")
+
+    # Read each row of data after the header
+    for row in csvreader:
+        print(row)
