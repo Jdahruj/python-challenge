d001752ac05f8e2b9ddd77e5ad2bdc44a87b9871
Joe Dahruj <joedahruj@me.com>
2020-03-16 20:56:54 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,5 +1,21 @@
+import os
 import pandas as pd
 
-df = pd.read_csv('D:/Documents/GitHub/python-challenge/budget_data.csv')
+df = pd.read_csv('budget_data.csv')
 
-df['Date']
+print("Total Months: " + str(df["Date"].size))
+print("Total: $" + str(df["Profit/Losses"].sum()))
+print("Average Change: $" + str(df["Profit/Losses"].mean()))
+print("Greatest Increase in Profits: $" + str(df['Profit/Losses'].max()))
+print("Greatest Decrease in Profits: $" + str(df['Profit/Losses'].min()))
+
+file = open('output.txt', 'w+')
+
+file.write("Total Months: " + str(df["Date"].size))
+file.write("\nTotal: $" + str(df["Profit/Losses"].sum()))
+file.write("\nAverage Change: $" + str(df["Profit/Losses"].mean()))
+file.write("\nGreatest Increase in Profits: $" + str(df['Profit/Losses'].max()))
+file.write("\nGreatest Decrease in Profits: $" + str(df['Profit/Losses'].min()))
+
+
+file.close()
